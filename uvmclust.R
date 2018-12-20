d15fe9a05cff64490b9ddf0ec14714966c1cf3a6
smartgamer <htchd7ji@live.com>
2018-12-20 17:13:23 -0500

    c

--- uvmclust.R
+++ uvmclust.R
@@ -14,21 +14,29 @@ rnat[1:6,1:6]
 set.seed(1001)
 uvmClus1 <- kmeans(rnat, 2)
 uvmClus2 <- kmeans(rnat, 3)
-uvmClus5 <- kmeans(rnat, 5)
+uvmClus5 <- kmeans(rnat, 5, nstart = 20)
 
 uvmClus1$cluster
 uvmClus1$size
 uvmClus1$centers
 
-table(uvmClus1$cluster, rnat$"8314")
-
-rnat$"8314"
-rnat$"10431"
+which(colnames(rnat)=="8314") #find which column is BAP1 (Entrez geneid=8314)
+table(uvmClus1$cluster, rnat[,1387])
 
 
+"8314" %in% colnames(rnat)
 
+head(rnat[,1387])
 
+library(ggplot2)
+uvmClus1$cluster <- as.factor(uvmClus1$cluster)
+rnatd=as.data.frame(rnat)
+ggplot(rnatd, aes(rnatd$"1387", rnatd$"1388", color = uvmClus1$cluster)) + geom_point()
 
+ggplot(rnatd, aes(rnatd$"1387", rnatd$"1388", color = uvmClus5$cluster)) + geom_point()
 
+colnames(rnat)[1388]
+rnatd[1387:1388]
 
-
+rnatd$cluster=uvmClus1$cluster
+rnatd[c(1387:1388, 20532)]
